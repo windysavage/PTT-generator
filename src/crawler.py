0eadf92a8279fec44cf54e73b813eec9cefe94c5
RH <water92001@gmail.com>
2021-12-12 12:10:28 +0800

    add until time to stop crawling

--- src/crawler.py
+++ src/crawler.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import argparse
 import requests
+from datetime import datetime
 
 from tqdm import tqdm
 from bs4 import BeautifulSoup
@@ -25,25 +26,35 @@ output_types = {
 
 
 class PttCrawler():
-    def __init__(self, topic, n_pages=None):
+    def __init__(self, topic, until):
         self.topic = topic
-        self.n_pages = n_pages
+        self.until = until
 
     def crawl(self):
         home_url = f"https://www.ptt.cc/bbs/{self.topic}/index.html"
-        page_urls = self._get_page_url(home_url) + [home_url]
+        page_urls = self._get_page_url(home_url)
+        page_urls.reverse()
+
         articles = self._get_article(page_urls)
 
         for article in articles:
-            article["title"] = article["title"].replace("\n", "")
+            article["title"] = article.get("title", "").replace("\n", "")
 
         return articles
 
     def _get_content(self, article):
+        cont = True
+
         title_div = article.select('.title')[0]
         title = title_div.text
 
-        article_url = article.select("a")[0].get("href")
+        # deleted articles didn't have url.
+        try:
+            article_url = article.select("a")[0].get("href")
+        except Exception as e:
+            logger.info("This article has been deleted.")
+            logger.error(e)
+            return {}, cont
 
         res = self.rs.get("http://www.ptt.cc" + article_url)
         soup = BeautifulSoup(res.text, "html.parser")
@@ -56,35 +67,41 @@ class PttCrawler():
             if meta_line.select('.article-meta-tag')[0].text == "作者":
                 author = meta_line.select('.article-meta-value')[0].text
                 continue
-            # if meta_line.select('.article-meta-tag')[0].text == "標題":
-            #     title = meta_line.select('.article-meta-value')[0].text
-            #     continue
             if meta_line.select('.article-meta-tag')[0].text == "時間":
                 publish_time = meta_line.select('.article-meta-value')[0].text
                 continue
+
+        if publish_time != "" and datetime.strptime(publish_time, "%a %b %d %H:%M:%S %Y") < self.until:
+            print(publish_time)
+            print(datetime.strptime(publish_time, "%a %b %d %H:%M:%S %Y"))
+            cont = False
+
         return{
             "url": article_url,
             "author": author,
             "title": title,
             "publish_time": publish_time
-        }
+        }, cont
 
     def _get_article(self, urls):
         all_results = []
-        for url in tqdm(urls):  # page
 
+        for url in tqdm(urls):  # page
             res = self.rs.get(url)
             soup = BeautifulSoup(res.text, "html.parser")
 
             articles = soup.select('.r-ent')
             page_results = []
+
             for article in articles:  # article
-                page_results.append(self._get_content(article))
+                content, cont = self._get_content(article)
+                page_results.append(content)
+
+                if not cont:
+                    all_results.extend(page_results)
+                    return all_results
 
             all_results.extend(page_results)
-
-            # except Exception as e:
-            #     logger.error(e)
 
         return all_results
 
@@ -112,8 +129,7 @@ class PttCrawler():
                 "index[0-9][0-9]",
                 "index[0-9]"]
             last_idx = re.findall("|".join(ptns), last_page)
-            last_idx = last_idx[0].replace(
-                "index", "") if self.n_pages is None else self.n_pages
+            last_idx = last_idx[0].replace("index", "")
             urls = [
                 f"https://www.ptt.cc/bbs/Gossiping/index{i}.html" for i in range(1, int(last_idx) + 1)]
             break
@@ -127,9 +143,13 @@ if __name__ == "__main__":
     parser.add_argument("--output-dir", type=str, default="./data")
     parser.add_argument("--output-type", type=str,
                         default="json", choices=["json"])
+    parser.add_argument("--until", type=str,
+                        default="2021-12-12@11-20-00")
     args = parser.parse_args()
 
-    crawler = PttCrawler(topic=args.topic, n_pages=1)
+    until = datetime.strptime(args.until, "%Y-%m-%d@%H-%M-%S")
+
+    crawler = PttCrawler(topic=args.topic, until=until)
     results = crawler.crawl()
     output_types[args.output_type](
         contents=results, output_dir=args.output_dir)
--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,11 +1,12 @@
 import pytest
+from datetime import datetime, timedelta
 
 from crawler import PttCrawler
 
 
 def test_get_page():
-    n_pages = 0
-    crawler = PttCrawler(topic="Gossiping", n_pages=n_pages)
+    until = datetime.now() - timedelta(minutes=30)
+    crawler = PttCrawler(topic="Gossiping", until=until)
     contents = crawler.crawl()
     assert isinstance(contents, list)
     assert len(contents) > 0
