7b8ac24f1fdbc741da1fff016ba5a9c3c4da098c
RH <water92001@gmail.com>
2021-12-12 12:26:13 +0800

    consider time zone info

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,11 +1,13 @@
 import pytest
-from datetime import datetime, timedelta
+from datetime import datetime, timezone, timedelta
 
 from crawler import PttCrawler
 
 
 def test_get_page():
-    until = datetime.now() - timedelta(minutes=30)
+    until = datetime.now(timezone.utc) + \
+        timedelta(hours=8) - timedelta(minutes=15)
+    until = until.replace(tzinfo=None)
     crawler = PttCrawler(topic="Gossiping", until=until)
     contents = crawler.crawl()
     assert isinstance(contents, list)
